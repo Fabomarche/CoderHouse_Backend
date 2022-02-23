fc1e406b8c28fe80119bb86677812ab0fcd7423e
Fabomarche <fabriciomarche@hotmail.com>
2022-02-23 14:12:53 -0300

    3er entrega Proyecto final refactoreo

---
+++ src/dao/chat.js
@@ -0,0 +1,41 @@
+import Container from "../../containers/Container.js";
+import messages from "../model/messages.js"
+import { normalize, schema } from 'normalizr'
+
+export default class Chat extends Container{
+    constructor(){
+        super(
+            'chats',
+            
+        )
+    }
+
+    getAllNormalizedChats = async() => {
+        try{
+            let documents = await this.collection.find()
+            let chats = {id:'chats', chats:documents}
+            console.log(chats)
+            const authorSchema = new schema.Entity('authors')
+            const postsSchema = new schema.Entity('posts',{
+                authors:authorSchema,
+            })
+            const normalizedData = normalize(chats, postsSchema)
+            console.log(JSON.stringify(normalizedData,null,2))
+            return {status:"success", payload:normalizedData}
+        }catch(err){
+            return {status:"error", error:err}
+        }
+    }
+
+
+    async saveChat(chat){
+        try{
+            let result = await this.collection.create(chat)
+            return {status: "success", payload:result}
+        }catch(err){
+            console.log(`No se pudo escribir el archivo ${err}`)
+            return {status:"error", message:"Error al agregar chat "+err}
+        }
+    }
+
+}
